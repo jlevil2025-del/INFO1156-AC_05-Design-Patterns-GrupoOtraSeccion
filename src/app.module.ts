9acb9263734182a8173d28186c8c30c49d8dc206
21kiqz <alanbernales455@gmail.com>
2026-05-21 01:51:42 -0400

    feat: implementar creational pattern(Factory Method) para notificaciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common"
 import { PostsModule } from "@/posts/posts.module"
 import { PrismaModule } from "@/prisma/prisma.module"
+// 1. Agregamos la importación con el mismo formato de alias:
+import { NotificacionesModule } from "@/notificaciones/notificaciones.module"
 
 @Module({
-    imports: [PrismaModule, PostsModule],
+
+    imports: [PrismaModule, PostsModule, NotificacionesModule],
 })
-export class AppModule {}
+export class AppModule { }
