07fc13eedf05238deec4a6271e07435e9b5752dd
Jaime Levil <jlevil2025@alu.uct.cl>
2026-05-28 22:31:46 -0400

    fix: resolver conflicto en AppModule fusionando AppController y NotificacionesModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,15 @@
 import { Module } from "@nestjs/common"
 import { PostsModule } from "@/posts/posts.module"
 import { PrismaModule } from "@/prisma/prisma.module"
-<<<<<<< HEAD
-// 1. Agregamos la importación con el mismo formato de alias:
+import { AppController } from "@/app.controller"
 import { NotificacionesModule } from "@/notificaciones/notificaciones.module"
 
 @Module({
-
-    imports: [PrismaModule, PostsModule, NotificacionesModule],
-=======
-import { AppController } from "@/app.controller"
-
-@Module({
-    imports: [PrismaModule, PostsModule],
+    imports: [
+        PrismaModule,
+        PostsModule,
+        NotificacionesModule
+    ],
     controllers: [AppController],
->>>>>>> 8814e04563b4063d0258e4affa524b0460370fc8
 })
 export class AppModule { }
